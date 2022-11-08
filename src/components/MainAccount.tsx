b62cc167599b1bbab6741b003881fefc09a28cfc
thanhvk <thanhvk.w3s@gmail.com>
2022-11-08 17:41:27 +0700

    first commit

---
+++ src/components/MainAccount.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState, useCallback } from 'react';
+import ReactJson from 'react-json-view';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+
+import './styles.css';
+
+const parser = (accountInfo: any) => {
+  if (!accountInfo) return accountInfo;
+
+  const parsedAccountInfo = {...accountInfo};
+  parsedAccountInfo.owner = parsedAccountInfo?.owner?.toBase58();  
+  return parsedAccountInfo;
+}
+
+const MainAccount = () => {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+  const [accountInfo, setAccountInfo] = useState<any>(null);
+
+  const getAccountInfo = useCallback(async () => {
+    if (!publicKey) return null;
+
+    const result = await connection.getAccountInfo(publicKey);
+    setAccountInfo(result);
+  }, [connection, publicKey]);
+
+  useEffect(() => {
+    getAccountInfo();
+  }, [getAccountInfo]);
+
+  if (!accountInfo) return null;
+
+  return (
+  <div>
+    <div className='method'><code className='method'>getAccountInfo</code></div>
+
+    <div className='row'>
+      <div className='col-12'>
+        <ReactJson displayObjectSize={false} name={false} collapsed={1} src={parser(accountInfo)} />
+      </div>
+      <div className='col-12'>
+        <ReactJson displayObjectSize={false} name={false} collapsed={1} src={accountInfo} />
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default MainAccount;
---
+++ src/components/TokenAccountList.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import ReactJson from 'react-json-view';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+
+import './styles.css';
+
+const parser = (tokenAccount: any) => {
+  if (!tokenAccount) return tokenAccount;
+
+  const parsedAccountInfo = {
+    ...tokenAccount.account.data.parsed.info,
+    programOwner: tokenAccount.account.owner.toBase58(),
+  };
+
+  return parsedAccountInfo;
+}
+
+const TokenAccountList = () => {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+  const [tokenAccountResult, setTokenAccountResult] = useState<any>(null);
+
+  const getTokenAccounts = useCallback(async () => {
+    if (!publicKey) return null;
+
+    const result = await connection.getParsedTokenAccountsByOwner(
+      publicKey,
+      {
+        programId: TOKEN_PROGRAM_ID,
+      }
+    );
+
+    setTokenAccountResult(result);
+  }, [connection, publicKey]);
+
+  useEffect(() => {
+    getTokenAccounts();
+  }, [getTokenAccounts]);
+
+  const parsedTokenAccounts = useMemo(() => {
+    if (!tokenAccountResult) return tokenAccountResult;
+
+    return tokenAccountResult.value.map((token: any) => {
+      return parser(token);
+    })
+  }, [tokenAccountResult]);
+
+  if (!tokenAccountResult) return null;
+
+  return (
+  <div>
+    <div className='method'><code className='method'>getParsedTokenAccountsByOwner</code></div>
+
+    <div className='row'>
+      <div className='col-12'>
+        <ReactJson displayObjectSize={false} name={false} collapsed={1} src={parsedTokenAccounts} />
+      </div>
+      <div className='col-12'>
+        <ReactJson displayObjectSize={false} name={false} collapsed={1} src={tokenAccountResult} />
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default TokenAccountList;
